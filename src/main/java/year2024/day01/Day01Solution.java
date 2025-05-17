63806fc7ebd849dc99d42f8952f9df5a4515252d
Zeus <zeus.builds101@gmail.com>
2025-05-17 20:41:36 +0530

    main method for day 01 of 2024

---
+++ src/main/java/year2024/day01/Day01Solution.java
@@ -0,0 +1,20 @@
+package year2024.day01;
+
+import year2024.day01.utils.InputParser;
+
+import java.io.InputStream;
+
+public class Day01Solution {
+    public static void main(String[] args) {
+        InputStream input = Day01Solution.class.getClassLoader().getResourceAsStream("year2024/day01/input.txt");
+        if (input == null) {
+            System.out.println("Input file not found.");
+            return;
+        }
+        var locationId = InputParser.safeInput(input);
+        int part1Solution = Part1Solver.solve(locationId);
+        int part2Solution = Part2Solver.solve(locationId);
+        System.out.println("Part 1: " + part1Solution);
+        System.out.println("Part 2: " + part2Solution);
+    }
+}
