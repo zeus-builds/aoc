69ff3dd78d820e0ba49ae8260d7f3e2c8ddfd1a7
Zeus <zeus.builds101@gmail.com>
2025-05-17 18:14:18 +0530

    add sample test case for part1 of day 01 of 2024

---
+++ src/test/java/year2024/day01/Day01SolverTest.java
@@ -0,0 +1,23 @@
+package year2024.day01;
+
+import org.junit.jupiter.api.Test;
+import year2024.day01.utils.InputParser;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class Day01SolverTest {
+
+    @Test
+    public void sampleTestPart1() {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("year2024/day01/sample.txt");
+
+        assertNotNull(input, "Test file not found");
+
+        var pairs = InputParser.safeInput(input);
+        int result = Day01Part1Solver.solve(pairs);
+        assertEquals(11, result);
+    }
+}
